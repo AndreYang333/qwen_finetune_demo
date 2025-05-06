cba4afa6e16c230c30635b8fabbc87f225c1ac6c
Andre Yang <minjieyang333@gmail.com>
2025-05-05 20:48:54 -0400

    111

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import yaml
 from model.trainer import train
 
 if __name__ == '__main__':
-    with open("configs/train_config.yaml", "r", encoding="utf-8") as f:
+    with open("configs/train_configs.yaml", "r", encoding="utf-8") as f:
         cfg = yaml.safe_load(f)
     train(cfg)
